62e048c53a0ac6663c3122e699d2b5ba26ebaf7e
romina <rominacannizzaro@gmail.com>
2023-10-27 11:53:28 +0300

    refactor: change text content acccording to episode and series view

--- src/components/VideoDetails/VideoDetails.tsx
+++ src/components/VideoDetails/VideoDetails.tsx
@@ -9,6 +9,8 @@ import useBreakpoint, { Breakpoint } from '#src/hooks/useBreakpoint';
 import Image from '#components/Image/Image';
 import { testId } from '#src/utils/common';
 
+type SecondaryMetadataWithTitle = { props: { children: Array<string> } };
+
 type Props = {
   title: string;
   description: string;
@@ -45,6 +47,13 @@ const VideoDetails: React.FC<Props> = ({
 
   const videoType = isEpisode ? 'episode' : 'series';
 
+  const episodeTitle = (secondaryMetadata as SecondaryMetadataWithTitle)?.props?.children[2];
+  const makeEpisodeSubtitle = (title: string, primaryMetadata: string) => {
+    const episodes = primaryMetadata?.split('•').pop();
+    const episodeSubtitle = title + ' - ' + episodes;
+    return episodeSubtitle;
+  };
+
   return (
     <div data-testid={testId('cinema-layout')}>
       <div className={styles.video} data-testid={testId('video-details')}>
@@ -53,10 +62,18 @@ const VideoDetails: React.FC<Props> = ({
             <Image className={classNames(styles.poster, { [styles.episode]: isEpisode, [styles.series]: !isEpisode })} image={image} alt={title} width={1280} />
           </div>
           <div className={classNames({ [styles.infoEpisode]: isEpisode, [styles.infoSeries]: !isEpisode })}>
-            <h2 className={classNames(styles.title, { [styles.episode]: isEpisode, [styles.series]: !isEpisode })}>{title}</h2>
+            {secondaryMetadata && isEpisode ? (
+              <h2 className={classNames(styles.title, { [styles.episode]: isEpisode, [styles.series]: !isEpisode })}>{episodeTitle}</h2>
+            ) : (
+              <h2 className={classNames(styles.title, { [styles.episode]: isEpisode, [styles.series]: !isEpisode })}>{title}</h2>
+            )}
             <div className={styles.metaContainer}>
-              <div className={styles.primaryMetadata}>{primaryMetadata}</div>
-              {secondaryMetadata && <div className={styles.secondaryMetadata}>{secondaryMetadata}</div>}
+              {primaryMetadata && isEpisode ? (
+                <div className={styles.primaryMetadata}>{makeEpisodeSubtitle(title, primaryMetadata as string)}</div>
+              ) : (
+                <div className={styles.primaryMetadata}>{primaryMetadata}</div>
+              )}
+              {secondaryMetadata && isEpisode ? '' : <div className={styles.secondaryMetadata}>{secondaryMetadata}</div>}
             </div>
             <CollapsibleText text={description} className={styles.description} maxHeight={isMobile ? 60 : 'none'} />
             <div className={classNames(styles.buttonBar, { [styles.episode]: isEpisode, [styles.series]: !isEpisode })}>
