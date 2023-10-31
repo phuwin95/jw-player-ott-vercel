52dd8c2dff5c6cf51b67ec7f095b3fda67515512
romina <rominacannizzaro@gmail.com>
2023-10-31 13:36:31 +0200

    chore: remove unnecessary comment in PreviousButton

--- src/icons/PreviousButton.tsx
+++ src/icons/PreviousButton.tsx
@@ -5,11 +5,6 @@ import createIcon from './Icon';
 export default createIcon(
   '0 0 19 29',
   <g>
-    <path
-      d="M16 3L4 14.5714L16 26.1429" /*d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"*/
-      stroke="variables.$seiska-yellow"
-      strokeWidth="5"
-      strokeLinecap="round"
-    />
+    <path d="M16 3L4 14.5714L16 26.1429" stroke="variables.$seiska-yellow" strokeWidth="5" strokeLinecap="round" />
   </g>,
 );
